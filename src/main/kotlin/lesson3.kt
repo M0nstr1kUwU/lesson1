844f58bf1f25c6c64f01b05710fbb02eede35460
M0nstr1kUwU <meowgrief@gmail.com>
2025-10-14 20:55:25 +0300

    Re
    DZ

--- src/main/kotlin/lesson3.kt
+++ src/main/kotlin/lesson3.kt
@@ -1,4 +1,5 @@
 import kotlin.random.Random
+import kotlin.math.roundToInt
 
 // БАЗОВЫЙ КЛАСС (РОДИТЕЛЬСКИЙ) - делаем его open для наследования
 open class Character(
@@ -22,7 +23,7 @@ open class Character(
         get() = _attack
 
     open val isAlive: Boolean
-        get() = _health <= 0
+        get() = _health > 0
 
     init {
         println("Создан персонаж $name")
@@ -33,10 +34,10 @@ open class Character(
 
         val actualDamage = damage.coerceAtLeast(0)
         _health -= actualDamage
-        println("$name получил $actualDamage урона")
+        println("> $name получил $actualDamage урона")
 
         if (_health <= 0){
-            println("$name погиб")
+            println("> $name погиб")
             return
         }
     }
@@ -45,19 +46,19 @@ open class Character(
         if (!isAlive) return
         val healAmount = amount.coerceAtLeast(0)
         _health = (_health + healAmount).coerceAtMost(_maxHealth)
-        println("$name похилился на $healAmount HP")
+        println("> $name похилился на $healAmount HP")
     }
 
     open fun attack(target: Character){
         if (!isAlive || !target.isAlive) return
         val damage = calculateDamage(_attack)
-        println("$name атакует ${target.name}")
+        println("> $name атакует ${target.name}")
         target.takeDamage(damage)
     }
 
     fun printStatus(){
         val status = if (isAlive) "Жив" else "Мёртв"
-        println("$name: $_health/$_maxHealth HP | ATK: $_attack ($status)")
+        println("| $name: $_health/$_maxHealth HP | ATK: $_attack ($status)")
     }
 }
 
@@ -70,7 +71,7 @@ class Warrior(name: String, maxHealth: Int, baseAttack: Int): Character(name, ma
     override fun takeDamage(damage: Int) {
         if (!isAlive) return
         val reducedDamage = (damage - armor).coerceAtLeast(0)
-        println("$name поглощает $armor урона!")
+        println("> $name поглощает $armor урона!")
         super.takeDamage(reducedDamage) // super - вызов метода родительского класса
     }
 
@@ -80,17 +81,18 @@ class Warrior(name: String, maxHealth: Int, baseAttack: Int): Character(name, ma
         if (_health - cost >= 1){
             _health -= cost
             val damage = calculateDamage(_attack * 2)
-            println("$name входит в режим Берсерка!")
-            println("$name атакует ${target.name} с удвоенным уроном")
+            println("> $name входит в режим Берсерка!")
+            println("> $name атакует ${target.name} с удвоенным уроном")
+
             target.takeDamage(damage)
         }else{
-            println("$name, а ниче тот факт хп не хватает?")
+            println("> $name, а ниче тот факт хп не хватает?")
             attack(target)
         }
     }
 }
 
-// КЛАСС-НАСЛЕДНИК WARRRIOR
+// КЛАСС-НАСЛЕДНИК MAGE
 class Mage(name: String, maxHealth: Int, baseAttack: Int): Character(name, maxHealth, baseAttack){
     private val maxMana: Int = 100
     private var mana: Int = maxMana
@@ -99,7 +101,7 @@ class Mage(name: String, maxHealth: Int, baseAttack: Int): Character(name, maxHe
     override fun takeDamage(damage: Int) {
         if (!isAlive) return
         val actualDamage = (damage + 10).coerceAtLeast(0)
-        println("$name получает увеличенный урон: -$actualDamage HP!")
+        println("> $name получает увеличенный урон: -$actualDamage HP!")
         super.takeDamage(actualDamage) // super - вызов метода родительского класса
     }
 
@@ -109,11 +111,11 @@ class Mage(name: String, maxHealth: Int, baseAttack: Int): Character(name, maxHe
         if (mana - cost >= 0){
             mana -= cost
             val damage = calculateDamage(_attack + 5)
-            println("$name вкладывает ману в атаку!")
-            println("$name атакует ${target.name} с увеличенным уроном уроном: $damage")
+            println("> $name вкладывает ману в атаку!")
+            println("> $name атакует ${target.name} с увеличенным уроном уроном: $damage")
             target.takeDamage(damage)
         }else{
-            println("$name ты слишком устал для усиленной атаки!")
+            println("> $name ты слишком устал для усиленной атаки!")
             attack(target)
         }
     }
@@ -123,12 +125,12 @@ class Mage(name: String, maxHealth: Int, baseAttack: Int): Character(name, maxHe
         val cost = 10
         if (mana - cost >= 0){
             mana -= cost
-            val damage = calculateDamage(_attack + 5)
-            println("$name кастует в ${target.name} FIREBALL!")
-            println("$name нанёс ${target.name} $damage урона")
+            val damage = calculateDamage(_attack * Random.nextDouble(1.0, 3.0).roundToInt())
+            println("> $name кастует в ${target.name} FIREBALL!")
+            println("> $name нанёс ${target.name} $damage урона")
             target.takeDamage(damage)
         }else{
-            println("$name ты слишком устал для такой атаки!")
+            println("> $name ты слишком устал для такой атаки!")
             attack(target)
         }
     }
@@ -143,16 +145,51 @@ fun calculateDamage(baseAttack: Int): Int{
 fun main(){
     println(">>>!БОЙ НАЧИНАЕТСЯ!<<<")
     val player = Mage("M0nstr1k", 100, 20)
-    val enemy = Warrior("Lord", 100, 20)
-    println("\n>==================================<")
-    player.printStatus()
-    enemy.printStatus()
-    println(">==================================<\n")
+    val enemy = Warrior("Lord Demons", 100, 20)
+    var turns = 1
 
-    player.attack(enemy)
-    enemy.attack(player)
-    player.amplifierDamage(enemy)
-    enemy.berserk(player)
-    player.fireball(enemy)
-    enemy.heal(50)
+    while (player.isAlive || enemy.isAlive){
+        println("\nTurns: $turns")
+
+        println("\n>=========================================<")
+        player.printStatus()
+        enemy.printStatus()
+        println(">=========================================<\n")
+        println("Actions: 1-Attack, 2-Heal, 3-Amplifier-Attack, 4-Fireball: ")
+
+        val action_player = readln().toInt()
+        println(">------------------------<")
+        if (action_player == 1){
+            player.attack(enemy)
+        }
+        else if (action_player == 2){
+            player.heal(50)
+        }
+        else if (action_player == 3){
+            player.amplifierDamage(enemy)
+        }
+        else if (action_player == 4){
+            player.fireball(enemy)
+        }
+        else{
+            println("Неверный выбор!")
+            continue
+        }
+
+        println()
+
+        val action_enemy = Random.nextInt(1, 4)
+        if (action_enemy == 1){
+            enemy.attack(player)
+        }
+        else if (action_enemy == 2){
+            enemy.heal(50)
+        }
+        else if (action_enemy == 3){
+            enemy.berserk(player)
+        }
+        println(">------------------------<")
+
+        turns += 1
+    }
 }
